a42da845e45146b280a600e244fefc695113570e
andersonrojas1998 <rojasanderson07@gmail.com>
2023-07-11 17:11:10 -0500

    size

--- public/lib/product.js
+++ public/lib/product.js
@@ -333,15 +333,16 @@ var loadAreaOptions = function(){
             {"className": "text-center", "targets": "_all"},
         ],
         columns:[
-            {"data": "nombre"},
-            {"data": "marca.nombre"},
-            {"data": "tipo_producto.descripcion"},
+            {"data": "producto"},
+            {"data": "marca"},
+            {"data": "tipo_producto"},
             {"data": "unidad_medida", render(unidad_de_medida){
-                return unidad_de_medida.nombre + " (" + unidad_de_medida.abreviatura + ")";
+                return unidad_de_medida;
             }},
-            {"data": "presentacion.nombre"},
+            {"data": "presentacion"},
             { "data": "cant_disponible",render(data){ return '<h4><label class="badge text-white badge-success">'+ data  +'</label></h4>'; }},
             {"data": "actions", render(data, ps, producto){
+                
                 let div = $('<div>',{
                     html: $("<a>", {
                         class: "btn_show_edit_product",
