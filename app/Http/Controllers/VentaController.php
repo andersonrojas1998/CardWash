e1796d15d44c36531f130597cd6d4302f9255f5d
andersonrojas1998 <rojasanderson07@gmail.com>
2023-10-03 10:30:01 -0500

    Orderby

--- app/Http/Controllers/VentaController.php
+++ app/Http/Controllers/VentaController.php
@@ -20,7 +20,7 @@ class VentaController extends Controller
 {
     public function index()
     {
-        $ventas = Venta::all();
+        $ventas = Venta::orderBy('id','DESC')->get()->take(150);
         $usuarios = users::select("users.*")->join("roles as r", "cargo", "r.id")->where("r.slug", "Lavador")->get();
         return view('venta.index', compact('ventas','usuarios'));
     }
